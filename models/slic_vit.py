2a101f32f885e623e9fc81a8f50ca22f3e80bac0
Snaky9 <EvanChen99@outlook.com>
2022-09-05 20:24:45 -0400

    saving images

--- models/slic_vit.py
+++ models/slic_vit.py
@@ -127,8 +127,9 @@ class SLICViT(nn.Module):
         # forward
         h, w = im.shape[:2]
         heatmap = self.get_heatmap(im, text)
-        # print("heatmap is ", type(heatmap), heatmap.shape, heatmap)
+        print("heatmap is ", type(heatmap), heatmap.shape, heatmap)
         heatimg = heatmap*255
+        print(heatimg)
         o_im = Image.fromarray(im).convert ('RGB')
         h_im = Image.fromarray(heatimg , 'L')
         o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/output/"+text+".jpg")
