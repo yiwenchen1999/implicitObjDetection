9de4a86da4134efcf9ac190971b6e3ac4e1faccd
Snaky9 <EvanChen99@outlook.com>
2022-09-06 01:45:28 -0400

    saving images

--- models/slic_vit.py
+++ models/slic_vit.py
@@ -132,8 +132,8 @@ class SLICViT(nn.Module):
         # print(heatimg)
         o_im = Image.fromarray(im).convert ('RGB')
         h_im = Image.fromarray(heatimg).convert ('RGB')
-        o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/output/"+text+".png")
-        h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/output/"+text+"_heat.png")
+        o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/output1/"+text+".png")
+        h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/output1/"+text+"_heat.png")
         print(text+" saved")
         bbox = self.box_from_heatmap(heatmap)
         bbox[:, ::2] = bbox[:, ::2] * w / w
