b1849c7c43793e42365dc0e653c6fff4c41cae52
Snaky9 <EvanChen99@outlook.com>
2022-09-12 10:46:34 -0400

    generating lego data

--- relevancy.py
+++ relevancy.py
@@ -65,6 +65,6 @@ if __name__=='__main__':
             # print(heatimg)
             o_im = Image.fromarray(im).convert ('RGB')
             h_im = Image.fromarray(heatimg).convert ('RGB')
-            o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/outputLego/"+filename+".png")
+            o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/outputLego/"+filename)
             h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/adaptingCLIPtesting/outputLego/"+filename[-4:]+"_heat.png")
             print(filename+" saved")
