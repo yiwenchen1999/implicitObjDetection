dcbed14d7265f0cb2758d9a76762ffa5831553ae
Snaky9 <EvanChen99@outlook.com>
2022-09-20 07:06:48 -0400

    adding heatmap to data

--- relevancy.py
+++ relevancy.py
@@ -53,7 +53,7 @@ model = model(**args).cuda()
 if __name__=='__main__':
     # path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/toybox-13/0/'
     # path = '/gpfs/data/ssrinath/ychen485/implicitSearch/NiceSlamTesting/Datasets/Demo/frames/color/'
-    path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/nerf/data/nerf_synthetic/chair/test/'
+    path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/nerf/data/nerf_synthetic/chair/val/'
     directories = os.listdir( path )
     for filename in directories:
         if filename[4] == ".":
