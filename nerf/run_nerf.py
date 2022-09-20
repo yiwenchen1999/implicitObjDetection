19c035f1ff3542b47ba2e78f4baf2503094c5c26
Snaky9 <EvanChen99@outlook.com>
2022-09-20 11:17:02 -0400

    testing saliency channel

--- nerf/run_nerf.py
+++ nerf/run_nerf.py
@@ -27,7 +27,7 @@ DEBUG = False
 def batchify(fn, chunk):
     """Constructs a version of 'fn' that applies to smaller batches.
     """
-    print("chunck: ", chunck)
+    print("chunck: ", chunk)
     if chunk is None:
         return fn
     def ret(inputs):
