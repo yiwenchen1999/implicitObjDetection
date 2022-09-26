8a4874501f920c37b91ffcef09f27f155f00b580
Mosquito <EvanChen99@outlook.com>
2022-09-26 12:04:41 -0400

    testing new nesf

--- nerf/run_nerf.py
+++ nerf/run_nerf.py
@@ -15,7 +15,8 @@ from run_nerf_helpers import *
 
 from load_llff import load_llff_data
 from load_deepvoxels import load_dv_data
-from load_blender import load_blender_data, load_Nesf_data
+from load_blender import load_blender_data
+from load_nesf import load_Nesf_data
 from load_LINEMOD import load_LINEMOD_data
 
 
