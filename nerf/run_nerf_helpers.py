adcec3cc91be3d1510bbdf5130bcd3c4c6d06b72
Mosquito <EvanChen99@outlook.com>
2022-09-27 10:37:59 -0400

    saliency nesf

--- nerf/run_nerf_helpers.py
+++ nerf/run_nerf_helpers.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Misc
-img2mse = lambda x, y : torch.mean((max(0,y-x)) ** 2)+torch.mean(((y-x)) ** 2)
+img2mse = lambda x, y : torch.mean((max(0,y-x)) ** 2)
 mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
 to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 
