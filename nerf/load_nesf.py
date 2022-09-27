04de7b2d2c687d915697f1818fe347a2df5ea10e
Mosquito <EvanChen99@outlook.com>
2022-09-27 06:11:25 -0400

    adding saliency to loader

--- nerf/load_nesf.py
+++ nerf/load_nesf.py
@@ -170,15 +170,15 @@ def load_Nesf_data(basedir, half_res=False, testskip=1, use_saliency = False):
         pose = dataloader[i]["pose"]
         imgs.append(img)
         poses.append(pose)
-        real_img = img*255
-        print(real_img.shape)
-        # heatmap = getHeatmap(model, real_img , "chair")
-        # saliency = heatmap*200
-        # print(saliency.shape)
-        o_im = Image.fromarray(np.uint8((img)*255))
-        # h_im = Image.fromarray(saliency).convert ('RGB')
+        real_img = np.uint8((img)*255)
+        # print(real_img.shape)
+        heatmap = getHeatmap(model, real_img , "chair")
+        saliency = heatmap*200
+        print(saliency.shape)
+        o_im = Image.fromarray(real_img)
+        h_im = Image.fromarray(saliency).convert ('RGB')
         o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/dataDemo/"+str(i)+".png")
-        # h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/dataDemo/"+str(i)+"_heat.png")
+        h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/dataDemo/"+str(i)+"_heat.png")
 
     imgs = (np.array(imgs)).astype(np.float32) # keep all 4 channels (RGBA)
     poses = np.array(poses).astype(np.float32)
