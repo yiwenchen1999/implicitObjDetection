b53124904c491843fc7e37f15c1ff84c228d152f
Mosquito <EvanChen99@outlook.com>
2022-10-04 08:03:59 -0400

    oct_seg

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import numpy as np
 import torch
 import torch.nn as nn
@@ -10,5 +11,15 @@ def seg(image, n_segments):
     w = int(image.shape[0]/seg)
     h = int(image.shape[1]/seg)
     print("w,h:",w,h)
-    mask = np.ones((w,h))
+    
     plate = np.zeros(image.shape)
+    n = 0
+    for i in range(seg):
+        for j in range(seg):
+            if n != 0:
+                mask = n*np.ones((w,h))
+                print("mask")
+                print(mask)
+                plate[i*w:(i+1)*w, j*h:(j+1)*h] = mask
+                print("plate:")
+                print(plate)
