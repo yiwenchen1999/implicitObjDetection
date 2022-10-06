b45997963ee3def7ef85fecdedd99f1207afcc81
Mosquito <EvanChen99@outlook.com>
2022-10-06 15:20:10 -0400

    sliding windows

--- relevancy.py
+++ relevancy.py
@@ -70,9 +70,9 @@ if __name__=='__main__':
             # print(heatimg)
             o_im = Image.fromarray(im).convert ('RGB')
             h_im = Image.fromarray(heatimg).convert ('RGB')
-            o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg2/"+filename)
-            h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg2/"+filename[:-4]+"_heat_comp_511.png")
-            # np.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg1/"+filename[:-4]+"_heat", heatmap)
+            o_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg2c/"+filename)
+            h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg2c/"+filename[:-4]+"_heat_comp_55.png")
+            np.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/octseg1/"+filename[:-4]+"_heat_55", heatmap)
             # h_im.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/outputChair/"+filename[:-4]+"_heat.png")
             # np.save("/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/outputChair/"+filename[:-4]+"_heat", heatmap)
 
