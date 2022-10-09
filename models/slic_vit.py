e3985d26049ab1dd6ade0d4b7a4e38328442114a
Snaky9 <EvanChen99@outlook.com>
2022-10-09 13:55:23 -0400

    generate clipmap test

--- models/slic_vit.py
+++ models/slic_vit.py
@@ -158,7 +158,7 @@ class SLICViT(nn.Module):
             print("keys:", key)
         masks, clip_features = self.get_mask_features(im)
         print("mask shape, feature shape:" , masks.shape, clip_features.shape)
-        featuremap = list(np.nan + np.zeros((masks.shape[0], masks.shape[1],clip_features.shape[1] ), dtype=np.float32))
+        featuremap = (np.nan + np.zeros((masks.shape[0], masks.shape[1],clip_features.shape[1] ), dtype=np.float32))
         # print("featuremap shape", featuremap.shape)
         print("i goes up to:", len(masks))
         for i in range(len(masks)):
@@ -166,7 +166,7 @@ class SLICViT(nn.Module):
             # print("mask:",mask.shape)
             features = clip_features[i]
             print("clip features: ", features.shape)
-            featuremap[i][mask] = features
+            featuremap[mask] = features
         featuremap = np.stack(featuremap, 0)
         print("heatmap:", featuremap.shape)
         return featuremap
