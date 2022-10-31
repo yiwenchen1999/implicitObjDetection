a843e198fa56b1fb7226e4e03bd35ffab7c7a9dc
Mosquito <EvanChen99@outlook.com>
2022-10-31 11:40:51 -0400

    trying to fix the clip network

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -1243,7 +1243,7 @@ def train(env, flag, test_file, i_weights):
     start = start + 1
     for i in trange(start, N_iters):
         # print(i)
-        if(i < 10000):
+        if(i < 80000):
             train_rgb = True
             train_clip = False
         else:
