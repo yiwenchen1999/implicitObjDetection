8dc174c1531d63fb475caf386838928ed2d3cd60
Mosquito <EvanChen99@outlook.com>
2022-10-31 23:34:30 -0400

    N importance

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -668,7 +668,7 @@ def config_parser(env, flag):
     # rendering options
     parser.add_argument("--N_samples", type=int, default=64, 
                         help='number of coarse samples per ray')
-    parser.add_argument("--N_importance", type=int, default=0,
+    parser.add_argument("--N_importance", type=int, default=128,
                         help='number of additional fine samples per ray')
     parser.add_argument("--perturb", type=float, default=1.,
                         help='set to 0. for no jitter, 1. for jitter')
