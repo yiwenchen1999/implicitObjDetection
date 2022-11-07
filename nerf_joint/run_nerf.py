b486f8c4e620a58111f7582d675f510ba2965e6a
Mosquito <EvanChen99@outlook.com>
2022-11-07 13:27:50 -0500

    remenber to save fine

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -180,7 +180,8 @@ def render_rays(ray_batch,
     #doing clip
     
     # if N_importance > 0 and ((not train_clip) or test_time):
-    if N_importance > 0 and ((not train_clip)):
+    # if N_importance > 0 and ((not train_clip)):
+    if N_importance > 0:
 
         if not test_time:
             rgb_map_0, disp_map_0, acc_map_0 = rgb_map, rgb_disp_map, rgb_acc_map
@@ -196,12 +197,6 @@ def render_rays(ray_batch,
         rgb_map, rgb_disp_map, rgb_acc_map, rgb_weights, _ = raw2outputs(raw_rgb, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest, saliency = False, clip = False)
 
     if train_clip:
-        z_vals_mid = .5 * (z_vals[...,1:] + z_vals[...,:-1])
-        z_samples = sample_pdf(z_vals_mid, rgb_weights[...,1:-1], N_importance, det=(perturb==0.), pytest=pytest)
-        z_samples = z_samples.detach()
-        z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
-        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
-
         _, raw_clips = network_query_fn(pts, viewdirs, network_clip) 
         if test_time:
             clip_map, clip_disp_map, clip_acc_map, _, _ = raw2outputs(raw_clips, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest, saliency = False, clip = True, raw_rgb = raw_rgb, joint = True)
@@ -1285,7 +1280,7 @@ def train(env, flag, test_file, i_weights):
     start = start + 1
     for i in trange(start, N_iters):
         # print(i)
-        if(i < 100000):
+        if(i < 40000):
             train_rgb = True
             train_clip = False
         else:
