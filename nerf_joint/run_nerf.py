e417f34a1997a141cf8bf94cb7dc6e7612227fda
Mosquito <EvanChen99@outlook.com>
2022-11-07 13:57:32 -0500

    smaller minibatch

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -674,7 +674,7 @@ def config_parser(env, flag):
                         help='learning rate')
     parser.add_argument("--lrate_decay", type=int, default=250, 
                         help='exponential learning rate decay (in 1000 steps)')
-    parser.add_argument("--chunk", type=int, default=1024*5, 
+    parser.add_argument("--chunk", type=int, default=1024*3, 
                         help='number of rays processed in parallel, decrease if running out of memory')
     parser.add_argument("--netchunk", type=int, default=1024*64, 
                         help='number of pts sent through network in parallel, decrease if running out of memory')
