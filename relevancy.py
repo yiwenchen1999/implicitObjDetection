e38957c88ed51e9147247d67647bc66e8cc379a7
Mosquito <EvanChen99@outlook.com>
2022-11-27 15:15:21 -0500

    attempting to encode room data

--- relevancy.py
+++ relevancy.py
@@ -60,7 +60,7 @@ model = model(**args).cuda()
 if __name__=='__main__':
     # print("runnning main fuction")
     # path = '/gpfs/data/ssrinath/toybox-13/0/'
-    path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/Demo/frames/color'
+    path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/Demo/frames/color/'
     # path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/nerf/data/nerf_synthetic/chair/train/'
     directories = os.listdir( path )
     i = 0
