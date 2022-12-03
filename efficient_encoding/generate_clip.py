4bc66ca58e6d2e0235948b76439f4a257e0d59db
Mosquito <EvanChen99@outlook.com>
2022-12-03 16:24:24 -0500

    testing on replica dataset

--- efficient_encoding/generate_clip.py
+++ efficient_encoding/generate_clip.py
@@ -23,9 +23,9 @@ if __name__=='__main__':
 
     #root_path = '/users/aren10/data/'
     #data_path = root_path + '0/'
-    root_path = '/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/Democlips/'
+    root_path = '/gpfs/data/ssrinath/ychen485/implicitSearch/room_0/Sequence_2/rgb/'
     # data_path = "/gpfs/data/ssrinath/ychen485/toybox-13/0/"
-    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/Demo/frames/color/"
+    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/results/replica/room0/frames/"
 
 
     directories = os.listdir(data_path)
@@ -46,18 +46,22 @@ if __name__=='__main__':
             # image_clip_feature_normalized = torch.tensor(np.load(root_path + image_id + "_image_clip_feature.npy")).cuda() #[256, 256, 768]
             #print(image_clip_feature_normalized)
             #image_clip_feature_normalized = (image_clip_feature_normalized - torch.unsqueeze(torch.min(image_clip_feature_normalized, dim = -1)[0], dim = -1)) / (torch.unsqueeze(torch.max(image_clip_feature_normalized, dim = -1)[0], dim = -1) - torch.unsqueeze(torch.min(image_clip_feature_normalized, dim = -1)[0], dim = -1))
-            query_map = model.verify(image_clip_feature_normalized, "chair", root_path).cpu().float().numpy()
-            #plt.imshow(query_map)
-            #plt.show()
-            query_map = np.squeeze(query_map)
-            query_map_remapped = (query_map - np.min(query_map)) / (np.max(query_map) - np.min(query_map))
-            r,c = np.shape(query_map_remapped)
-            query_map_3d = np.zeros((r,c,3))
-            query_map_3d[:,:,0] = query_map_remapped
-            query_map_3d[:,:,1] = query_map_remapped
-            query_map_3d[:,:,2] = query_map_remapped
-            plt.imshow(query_map_3d)
-            plt.imsave(root_path + filename[:-4]+"_heat.png", query_map_3d)
+            query_map = model.verify(image_clip_feature_normalized, "a stool", root_path).cpu().float().numpy()
+            # #plt.imshow(query_map)
+            # #plt.show()
+            # query_map = np.squeeze(query_map)
+            # query_map_remapped = (query_map - np.min(query_map)) / (np.max(query_map) - np.min(query_map))
+            # r,c = np.shape(query_map_remapped)
+            # query_map_3d = np.zeros((r,c,3))
+            # query_map_3d[:,:,0] = query_map_remapped
+            # query_map_3d[:,:,1] = query_map_remapped
+            # query_map_3d[:,:,2] = query_map_remapped
+
+            query_map = query_map.cpu().detach().numpy()
+            query_map = query_map.reshape(query_map.shape[0], query_map.shape[1])
+            plt.imshow(query_map)
+            # plt.imshow(query_map_3d)
+            plt.imsave(root_path + filename[:-4]+"_heat.png", query_map)
             # exit(0)
             
             
