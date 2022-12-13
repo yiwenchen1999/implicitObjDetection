76103f1af06ac01c9e559bbf2b9a5a22a6fac1e9
Snaky9 <EvanChen99@outlook.com>
2022-12-14 03:51:41 +0800

    editing llff loader

--- room_nerf/load_llff.py
+++ room_nerf/load_llff.py
@@ -60,7 +60,7 @@ def _minify(basedir, factors=[], resolutions=[]):
             print('Removed duplicates')
         print('Done')
             
-def _load_data_replica(basedir, factor=None, width=None, height=None, load_imgs=True):
+def _load_data_replica(basedir, factor=8, width=None, height=None, load_imgs=True):
     meta = load_from_json(basedir + "transforms.json")
     image_filenames = []
     # mask_filenames = []
