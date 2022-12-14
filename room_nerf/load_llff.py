505cbde4c06b17ec0af8b433c13aa0443603f118
Snaky9 <EvanChen99@outlook.com>
2022-12-14 18:36:11 +0800

    data compatebility

--- room_nerf/load_llff.py
+++ room_nerf/load_llff.py
@@ -196,7 +196,7 @@ def _load_data_replica(basedir, factor=8, width=None, height=None, load_imgs=Tru
     scale_factor = 1.0
     scale_factor /= torch.max(torch.abs(poses[:, :3, 3]))
     poses[:, :3, 3] *= scale_factor 
-    # imgs = np.stack(imgs, -1)  
+    imgs = np.stack(imgs, -1)  
     print("imgs: ", imgs.shape)
     print("poses: ", poses.shape)
 
