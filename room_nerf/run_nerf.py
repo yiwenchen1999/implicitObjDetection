10a79853184f4d35baf68ad084c8e4774a2e28ae
Snaky9 <EvanChen99@outlook.com>
2022-12-15 08:55:10 +0800

    train replica without batching

--- room_nerf/run_nerf.py
+++ room_nerf/run_nerf.py
@@ -759,18 +759,18 @@ def train():
             imageio.mimwrite(os.path.join(testsavedir, 'video.mp4'), to8b(rgbs), fps=30, quality=8)
 
             return
-    if args.render_query_video:
-        with torch.no_grad():
-            rgb_ests, rgb_disps, queries, clips_disps = render_query_video(args.root_path + "Nesf0_2D/" + args.text + "_clip_feature.npy", render_poses, hwf, K, args.chunk, render_kwargs_test, use_clip = True, train_clip = True, test_time = True)
-            # rgb_ests0, rgb_disps, queries0, clips_disps = render_query_video(args.root_path + "Nesf0_2D/" + args.text + "_clip_feature.npy", render_poses, hwf, K, args.chunk, render_kwargs_test, use_clip = True, train_clip = True, test_time =False)
+    # if args.render_query_video:
+    #     with torch.no_grad():
+    #         rgb_ests, rgb_disps, queries, clips_disps = render_query_video(args.root_path + "Nesf0_2D/" + args.text + "_clip_feature.npy", render_poses, hwf, K, args.chunk, render_kwargs_test, use_clip = True, train_clip = True, test_time = True)
+    #         # rgb_ests0, rgb_disps, queries0, clips_disps = render_query_video(args.root_path + "Nesf0_2D/" + args.text + "_clip_feature.npy", render_poses, hwf, K, args.chunk, render_kwargs_test, use_clip = True, train_clip = True, test_time =False)
 
-        imageio.mimwrite(args.root_path + "Nesf0_2D/queries.mp4", to8b(queries), fps=30, quality=8)
-        # imageio.mimwrite(args.root_path + "Nesf0_2D/queries0.mp4", to8b(queries0), fps=30, quality=8)
-        imageio.mimwrite(args.root_path + "Nesf0_2D/queries_disps.mp4", to8b(clips_disps / np.max(clips_disps)), fps=30, quality=8)
-        # imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_ests0.mp4", to8b(rgb_ests0), fps=30, quality=8)
-        imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_ests.mp4", to8b(rgb_ests), fps=30, quality=8)
-        imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_disps.mp4", to8b(rgb_disps / np.max(rgb_disps)), fps=30, quality=8)
-        return
+    #     imageio.mimwrite(args.root_path + "Nesf0_2D/queries.mp4", to8b(queries), fps=30, quality=8)
+    #     # imageio.mimwrite(args.root_path + "Nesf0_2D/queries0.mp4", to8b(queries0), fps=30, quality=8)
+    #     imageio.mimwrite(args.root_path + "Nesf0_2D/queries_disps.mp4", to8b(clips_disps / np.max(clips_disps)), fps=30, quality=8)
+    #     # imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_ests0.mp4", to8b(rgb_ests0), fps=30, quality=8)
+    #     imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_ests.mp4", to8b(rgb_ests), fps=30, quality=8)
+    #     imageio.mimwrite(args.root_path + "Nesf0_2D/rgb_disps.mp4", to8b(rgb_disps / np.max(rgb_disps)), fps=30, quality=8)
+    #     return
 
 
 
