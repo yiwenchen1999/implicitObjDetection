9dde0c0d1e3728de6993f1d78853a5ec8c9298c7
Snaky9 <EvanChen99@outlook.com>
2022-12-15 20:33:02 +0800

    dealing with coord

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -1443,6 +1443,7 @@ def train(env, flag, test_file, i_weights):
                     select_coords_clip[:, 1] = (select_coords[:, 1]*(clip.shape[1]/target.shape[1]))
                     print(select_coords_clip[0])
                     clip_s = clip[select_coords_clip[:, 0], select_coords_clip[:, 1]]
+                    print("selected shape:", clip_s.shape)
 
 
         if args.with_clip:
