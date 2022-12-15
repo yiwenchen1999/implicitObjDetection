80b8aff26fac6f666ba37e1fed9140283ad3ada1
Snaky9 <EvanChen99@outlook.com>
2022-12-15 20:42:06 +0800

    running the model now

--- nerf_joint/run_nerf.py
+++ nerf_joint/run_nerf.py
@@ -1435,16 +1435,16 @@ def train(env, flag, test_file, i_weights):
                 #clip_s             
                 if args.with_clip:
                     rgb_s = target[select_coords[:, 0], select_coords[:, 1]]
-                    print(select_coords.shape, select_coords[0])
+                    # print(select_coords.shape, select_coords[0])
                     select_coords_clip = select_coords
                     #remapping
-                    print(clip.shape[0]/target.shape[0], clip.shape[1]/target.shape[1])
-                    print(select_coords[:, 0]*(clip.shape[0]/target.shape[0]), select_coords[:, 1]*(clip.shape[1]/target.shape[1]))
+                    # print(clip.shape[0]/target.shape[0], clip.shape[1]/target.shape[1])
+                    # print(select_coords[:, 0]*(clip.shape[0]/target.shape[0]), select_coords[:, 1]*(clip.shape[1]/target.shape[1]))
                     select_coords_clip[:, 0] = (select_coords[:, 0]*(clip.shape[0]/target.shape[0]))
                     select_coords_clip[:, 1] = (select_coords[:, 1]*(clip.shape[1]/target.shape[1]))
-                    print(select_coords_clip[0])
+                    # print(select_coords_clip[0])
                     clip_s = clip[select_coords_clip[:, 0], select_coords_clip[:, 1]]
-                    print("selected shape:", clip_s.shape)
+                    # print("selected shape:", clip_s.shape)
                     # print(clip_s[0][4], clip[0])
 
 
