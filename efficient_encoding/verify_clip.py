ca98d79c090e5fc8c3b4e0aa0c883eceb289c73a
Snaky9 <EvanChen99@outlook.com>
2022-12-16 20:49:02 +0800

    verify clip should be working

--- efficient_encoding/verify_clip.py
+++ efficient_encoding/verify_clip.py
@@ -52,7 +52,7 @@ if __name__=='__main__':
             # query_map_3d[:,:,2] = query_map_remapped
 
             # query_map = query_map.cpu().detach().numpy()
-        # query_map = query_map.reshape(query_map.shape[0], query_map.shape[1])
+        query_map = query_map.reshape(query_map.shape[0], query_map.shape[1])
         plt.imshow(query_map)
         # plt.imshow(query_map_3d)
         plt.imsave(root_path + filename[:-4]+ text + "_heat.png", query_map)
