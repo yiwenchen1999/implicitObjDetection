a06fcf556a03d696d4c60d5e120382249646e21a
Snaky9 <EvanChen99@outlook.com>
2022-12-17 00:52:46 +0800

    quicker dot product

--- efficient_encoding/models/slic_vit.py
+++ efficient_encoding/models/slic_vit.py
@@ -239,5 +239,5 @@ class SLICViT(nn.Module):
             text_features_normalized = text_features_normalized.to(torch.float).cuda()
             image_features_normalized = image_features_normalized.to(torch.float).cuda()
             print(text_features_normalized.shape)
-            sem_img = torch.tensordot(image_features_normalized.float(), text_features_normalized.cpu(), dims=([2],[1])).detach().numpy()
+            sem_img = torch.tensordot(image_features_normalized.float(), text_features_normalized.cpu(), dims=([2],[0])).detach().numpy()
             return sem_img
