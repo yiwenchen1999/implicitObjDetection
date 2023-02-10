999f3f7a673f40341db959c4557e24804bf67f78
Mosquito <EvanChen99@outlook.com>
2023-02-10 11:55:28 -0500

    test on bags

--- efficient_encoding/generate_clip.py
+++ efficient_encoding/generate_clip.py
@@ -31,8 +31,8 @@ if __name__=='__main__':
     #cups schemes/
     # data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
     # root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
-    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/test_clip/cups/"
-    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/test_results/cups/"
+    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/test_clip/bags/"
+    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/test_results/bags/"
     # data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/room_nerf/logs/replica/renderonly_path_129999/"
     # root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/implicitObjDetection/room_nerf/logs/replica/renderonly_path_129999/"
 
