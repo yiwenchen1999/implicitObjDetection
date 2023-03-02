e033230fa084c26e1b1714a29e5aeb04e6c76c5f
Snaky9 <EvanChen99@outlook.com>
2023-03-02 16:50:24 -0500

    inspecting checkpoint saving

--- room_nerf/run_nerf.py
+++ room_nerf/run_nerf.py
@@ -946,7 +946,7 @@ def train():
     for i in trange(start, N_iters):
         time0 = time.time()
 
-        if(i < 50000):
+        if(i < 50001):
             train_rgb = True
             train_clip = False
         else:
