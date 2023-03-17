ea30c6ef336fd5fb6bb36be1815574303fbdd631
Mosquito <EvanChen99@outlook.com>
2023-03-17 10:52:53 -0400

    generating clip embedding for mic

--- efficient_encoding/generate_clip.py
+++ efficient_encoding/generate_clip.py
@@ -36,8 +36,8 @@ if __name__=='__main__':
     #cups schemes/
     # data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
     # # root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
-    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/images/"
-    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/images/"
+    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/mic_custom//images/"
+    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/mic_custom//images/"
     # data_path = os.path.join(args.data, "images")
     # root_path = os.path.join(args.data, "images")
     print(data_path)
