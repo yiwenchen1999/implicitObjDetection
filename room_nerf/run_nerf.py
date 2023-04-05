c225202c12936c42256b863191debeea6dd25f5c
Snaky9 <EvanChen99@outlook.com>
2023-04-05 16:24:49 -0400

    attempt to threshold on alpha

--- room_nerf/run_nerf.py
+++ room_nerf/run_nerf.py
@@ -402,7 +402,11 @@ def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=F
     if infer:
         alpha_clip = raw2alpha_clip(raw[...,-1] + noise, dists)  # [N_rays, N_samples]
         alpha_rgb = raw2alpha_rgb(raw_rgb[...,3] + noise, dists)  # [N_rays, N_samples]
+        print(alpha_rgb.shape)
+        print(alpha_rgb)
         alpha_clip = alpha_clip*alpha_rgb
+        result = (alpha_clip > 0.5) * alpha_clip #attempt to threshold alpha
+        alpha_clip = result #attempt to threshold alpha
         weights = alpha_clip * torch.cumprod(torch.cat([torch.ones((alpha_clip.shape[0], 1)), 1.-alpha_clip + 1e-10], -1), -1)[:, :-1]
         clip_map = torch.sum(weights[...,None] * clip, -2)  # [N_rays, 3]
     elif outputClip:
