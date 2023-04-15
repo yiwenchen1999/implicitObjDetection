0a7ddeda6b22cfff3fe2d7bd7a335aaed57f8f5f
Snaky9 <EvanChen99@outlook.com>
2023-04-15 10:14:21 -0400

    mic seg all testing

--- room_nerf/run_nerf.py
+++ room_nerf/run_nerf.py
@@ -1112,7 +1112,7 @@ def train():
     for i in trange(start, N_iters):
         time0 = time.time()
 
-        if(i < 100000):
+        if(i < 20000):
             train_rgb = True
             train_clip = False
         else:
