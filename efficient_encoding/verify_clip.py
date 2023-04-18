e63bc1525f404039aeb6f1994829ab4ed4e80c81
Snaky9 <EvanChen99@outlook.com>
2023-04-18 14:52:38 -0400

    threshold on clip score stand

--- efficient_encoding/verify_clip.py
+++ efficient_encoding/verify_clip.py
@@ -67,8 +67,8 @@ if __name__=='__main__':
 
 
             # save_query("the mic", image_clip_feature_normalized)
-            # save_query("stand", image_clip_feature_normalized)
-            save_query("wires", image_clip_feature_normalized)
+            save_query("stand", image_clip_feature_normalized)
+            # save_query("wires", image_clip_feature_normalized)
             # save_query("head", image_clip_feature_normalized)
             # save_query("the dog", image_clip_feature_normalized)
             # save_query("the ears", image_clip_feature_normalized)
