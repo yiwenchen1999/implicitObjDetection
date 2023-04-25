22595ec0b82ddf1047d007519d20e7a787a0cb80
Snaky9 <EvanChen99@outlook.com>
2023-04-25 19:49:47 -0400

    generate new

--- efficient_encoding/generate_clip.py
+++ efficient_encoding/generate_clip.py
@@ -36,8 +36,8 @@ if __name__=='__main__':
     #cups schemes/
     # data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
     # # root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/room_studio/images/"
-    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/train/"
-    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/train/"
+    data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/val/"
+    root_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/nerf_synthetic/mic/val/"
     # data_path = os.path.join(args.data, "images")
     # root_path = os.path.join(args.data, "images")
     # data_path = "/gpfs/data/ssrinath/ychen485/implicitSearch/test_clip/bags/"
